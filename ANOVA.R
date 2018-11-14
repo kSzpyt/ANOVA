1ca7b6c2c0b6eaafe6f3bb185e7d77642905c8b1
kSzpyt <karolo.szpyt@gmail.com>
2018-11-14 14:35:00 +0100

    progres, pierwszy rmd

--- ANOVA.R
+++ ANOVA.R
@@ -2,19 +2,20 @@ library(readxl)
 library(dplyr)
 library(tseries)
 library(car)
-#AGE   = wiek matki (w latach)
-#LWT   = Weight in Pounds at the Last Menstrual Period
-#RACE  = rasa (1 = biała, 2 = czarna, 3 = inna)
-#SMOKE = palenie w trakcie ciąży (1 = tak, 0 = nie)
-#PTL   = liczba wcześniejszych przedwczesnych porodów (0, 1,...)
-#HT    = nadciśnienie (1 = tak, 0 = nie)
-#UI    = presence of uterine irritability
-#FTV   = liczba wizyt u lekarza w pierwszym trymestrze ciąży (0, 1, 2,...)
-#BWT   = waga urodzonego dziecka (w gramach)
+library(sjstats)
+# AGE   = wiek matki (w latach)
+# LWT   = Weight in Pounds at the Last Menstrual Period
+# RACE  = rasa (1 = biała, 2 = czarna, 3 = inna)
+# SMOKE = palenie w trakcie ciąży (1 = tak, 0 = nie)
+# PTL   = liczba wcześniejszych przedwczesnych porodów (0, 1,...)
+# HT    = nadciśnienie (1 = tak, 0 = nie)
+# UI    = presence of uterine irritability
+# FTV   = liczba wizyt u lekarza w pierwszym trymestrze ciąży (0, 1, 2,...)
+# BWT   = waga urodzonego dziecka (w gramach)
 
 
 data <- read_xls("urodzenia.xls")
-
+View(data)
 summary(data) 
 attach(data)
 #normalność rozkłądu zmiennej zależnej
@@ -24,9 +25,9 @@ jarque.bera.test(BWT)
 
 attach(data)
 par(mfrow = c(2,3))
-boxplot(BWT ~ RACE, xlab = "RACE", ylab = "BWT")
+boxplot(BWT ~ RACE, xlab = "RACE", ylab = "BWT", main = "safasf")
 boxplot(BWT ~ SMOKE, xlab = "SMOKE", ylab = "BWT")
-boxplot(BWT ~ PTL, xlab = "PTL", ylab = "BWT")  # odrzucam
+boxplot(BWT ~ PTL, xlab = "PTL", ylab = "BWT")  
 boxplot(BWT ~ HT, xlab = "HT", ylab = "BWT")
 boxplot(BWT ~ UI, xlab = "UI", ylab = "BWT")
 boxplot(BWT ~ FTV, xlab = "FTV", ylab = "BWT")
@@ -39,6 +40,8 @@ hist(HT, breaks = 2)
 hist(UI, breaks = 2)
 hist(FTV, breaks = 6)
 
+
+
 a <- NULL
 b <- NULL
 for (x in 1:3) {
@@ -50,6 +53,14 @@ for (x in 1:3) {
     b[x+2] <- jarque.bera.test((data$BWT[which(data$SMOKE == x - 1)]))$p.value
   }
 }
+
+data %>%
+  group_by(SMOKE, RACE) %>%
+  summarise(pValue = shapiro.test(BWT)$p.value)
+
+data %>%
+  group_by(SMOKE, RACE) %>%
+  summarise(pValue = jarque.bera.test(BWT)$p.value)
 #w każdej z podgrup nie ma przesłanek do odrzucenia hipotezy o normalnośći rozkładu
 mean(a < 0.05)
 mean(b < 0.05)
@@ -73,8 +84,12 @@ fit <- aov(BWT ~ RACE * SMOKE, data)
 fit_s <- summary(fit)
 
 str(fit)
+str(fit_s)
 par(mfrow = c(2, 2))
 plot(fit)
+
+
+#fit_s[[1]][["Pr(>F)"]]
 
 #nie ma interakcji pomiędzy zmiennymi 
 interaction.plot(RACE, SMOKE, BWT)
@@ -85,10 +100,15 @@ TukeyHSD(fit)
 
 
 tapply(BWT, SMOKE, shapiro.test)
+tapply(BWT, RACE, shapiro.test)
 
 
+#efekty eksperymentalne
+eta_sq(fit)
 
+eta_sq(fit, partial = TRUE)
 
+omega_sq(fit)
 
 
 
